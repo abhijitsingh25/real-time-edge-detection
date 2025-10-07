0af51c0314e9044b151091a818877bfc7f86e486
abhijitsingh25 <134947350+abhijitsingh25@users.noreply.github.com>
2025-10-07 15:41:32 +0530

    Update MyGLRenderer.kt

--- app/src/main/java/com/example/edgedetection/MyGLRenderer.kt
+++ app/src/main/java/com/example/edgedetection/MyGLRenderer.kt
@@ -1,148 +1,25 @@
 package com.example.edgedetection
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.graphics.PixelFormat
-import android.content.pm.PackageManager
 import android.opengl.GLSurfaceView
-import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.edgedetection.databinding.ActivityMainBinding
-import java.nio.ByteBuffer
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
-class MainActivity : AppCompatActivity() {
+class MyGLRenderer : GLSurfaceView.Renderer {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var cameraExecutor: ExecutorService
-    private lateinit var glSurfaceView: GLSurfaceView
-    private lateinit var myRenderer: MyGLRenderer
+    // Native functions that will be implemented in C++
+    external fun onSurfaceCreated()
+    external fun onSurfaceChanged(width: Int, height: Int)
+    external fun onDrawFrame()
 
-    private external fun processFrame(yuvData: ByteArray, width: Int, height: Int)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        System.loadLibrary("edgedetection")
-
-        // Setup GLSurfaceView
-        glSurfaceView = binding.glSurfaceView
-        glSurfaceView.setEGLContextClientVersion(2)
-        glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0)
-        glSurfaceView.setZOrderOnTop(true)
-        glSurfaceView.holder.setFormat(PixelFormat.TRANSLUCENT)
-
-        myRenderer = MyGLRenderer()
-        glSurfaceView.setRenderer(myRenderer)
-        glSurfaceView.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
-
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                REQUIRED_PERMISSIONS,
-                REQUEST_CODE_PERMISSIONS
-            )
-        }
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        onSurfaceCreated()
     }
 
-    private fun allPermissionsGranted() =
-        REQUIRED_PERMISSIONS.all { ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(this, "Permissions not granted.", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        onSurfaceChanged(width, height)
     }
 
-    @SuppressLint("UnsafeOptInUsageError")
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-        cameraProviderFuture.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            val preview = Preview.Builder()
-                .setTargetResolution(android.util.Size(640, 480)) // smaller resolution for speed
-                .build()
-                .also {
-                    it.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
-                }
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor) { imageProxy ->
-                        val nv21 = imageProxy.toNV21()
-                        processFrame(nv21, imageProxy.width, imageProxy.height)
-                        glSurfaceView.requestRender()
-                        imageProxy.close()
-                    }
-                }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-        }, ContextCompat.getMainExecutor(this))
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
-    }
-
-    // Convert ImageProxy to NV21 byte array
-    private fun ImageProxy.toNV21(): ByteArray {
-        val yBuffer = planes[0].buffer
-        val uBuffer = planes[1].buffer
-        val vBuffer = planes[2].buffer
-
-        val ySize = yBuffer.remaining()
-        val uSize = uBuffer.remaining()
-        val vSize = vBuffer.remaining()
-
-        val nv21 = ByteArray(ySize + uSize + vSize)
-
-        // Y plane
-        yBuffer.get(nv21, 0, ySize)
-
-        // Interleave VU for NV21
-        var uvIndex = ySize
-        for (i in 0 until uSize) {
-            nv21[uvIndex++] = vBuffer.get(i)
-            nv21[uvIndex++] = uBuffer.get(i)
-        }
-        return nv21
-    }
-
-    companion object {
-        private const val TAG = "EdgeDetectionApp"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    override fun onDrawFrame(gl: GL10?) {
+        onDrawFrame()
     }
 }
